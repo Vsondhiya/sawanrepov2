fe50d9da6091005305cb29c7e1186b4cf027776f
Vsondhiya <vsondhiya@gmail.com>
2025-08-05 11:43:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-    subscription_id = "def13e29-f65a-496b-b10e-3cffca9a480f"
+    subscription_id = "bcf304d8-21b8-4358-9c77-54b5dd26f938"
     
 }
